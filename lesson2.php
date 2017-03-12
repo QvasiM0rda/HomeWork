ccff5f71be7006a3e30e2c8c0f99c38dd4916e7d
QvasiM0rda <darkrustam@gmail.com>
2017-03-12 23:33:15 +0300

    Lesson 2
    Homework lesson 2

---
+++ lesson2.php
@@ -0,0 +1,41 @@
+﻿<?php
+  $userNumber = $_GET ['userNumber'];
+  $userNumber = (int) $userNumber;
+
+  $number_1 = 1;
+  $number_2 = 1;
+  $number_3 = 0;
+
+  while (true) {
+  	if ($number_1 > $userNumber) {
+  		echo ' Число ' . $userNumber . ' не входит в числовой ряд.';
+  		break;
+  	}
+
+  	if ($number_1 == $userNumber) {
+  		echo ' Число ' . $userNumber . ' входит в числовой ряд.';
+  		break;
+  	}
+  	else {
+  		$number_3 = $number_1;
+  		$number_1 = $number_1 + $number_2;
+  		$number_2 = $number_3;
+  	}
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Домашнее задание ко второму уроку</title>
+	<meta charset="utf-8">
+</head>
+<body>
+  <form method="GET">
+  <label for="userNumber">Введите число: </label>
+  	<input type="text" name="userNumber" id="userNumber">
+    <br>
+    <input type="submit" name="submit">
+  </form>
+</body>
+</html>
