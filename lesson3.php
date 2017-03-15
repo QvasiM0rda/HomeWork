7cc5c561b97ec3eef5dc782a24d5fb4831ce51b5
QvasiM0rda <darkrustam@gmail.com>
2017-03-15 14:41:06 +0300

    Add files via upload

--- lesson3.php
+++ lesson3.php
@@ -12,7 +12,7 @@
       'Procyon lotor', 'Rangifer tarandus', 'Alces alces'
     ), 
     'South America' => array(
-      'Vultur gryphus ', 'Mephitidae', 'Puma concolor'
+      'Vultur gryphus', 'Mephitidae', 'Puma concolor'
     ), 
     'Australia' => array(
       'Sarcophilus laniarius', 'Macropus', 'Ornithorhynchus anatinus'
@@ -21,35 +21,39 @@
       'Arctocephalus gazella', 'Aptenodytes forsteri', 'Pagodroma nivea'
     )
   );
-//Находим парные имена, создаем два массива: $fantasticBeast, состоящий из континента и первой части имени; $secondNameArray - состоящий из второй части имени
-   foreach ($continents as $continentKey => $continent) {
+
+function fantasticBeast ($continents) {
+  foreach ($continents as $continentName => $animalArray) {
     $firstName = '';
-    foreach ($continent as $animalKey => $animalName) {
-      $spacePosition = strpos($animalName, ' '); //находим пробел, если есть, делим элемент на две части 
+    foreach ($animalArray as $animalKey => $animalName) {
+      $spacePosition = strpos($animalName, ' ');
       if (!empty($spacePosition)) {
         $firstName = $firstName . substr($animalName, 0, $spacePosition) . ',';
-        $secondNameArray[] = substr($animalName, $spacePosition);        
-      } 
+        $secondNameArray[] = substr($animalName, $spacePosition);
+      }
     }
-    $fantasticBeast[$continentKey] = explode(',', $firstName);
+    $firstNameArray[$continentName] = explode(',', $firstName);
   }
 
   shuffle($secondNameArray);
-  $arrayCounter = 0;
-//Вводим всё вместе, пробуем убрать пробел у последнего элемента
-  foreach ($fantasticBeast as $fantasticContinent => $firstNameArray) {
-    array_pop($firstNameArray);
-    echo '<h2>' . $fantasticContinent . '</h2>';
-    for ($i=0; $i<=count($firstNameArray) - 1; $i++) {
-      if ($i === count($firstNameArray) - 1) {
-        echo $firstNameArray[$i] . $secondNameArray[$arrayCounter];
+  $arrayCount = 0;
+
+  foreach ($firstNameArray as $continentName => $animalArray) {
+    $toOutput = '';
+    array_pop($animalArray);
+    for ($i=0; $i<=count($animalArray) - 1; $i++) {
+      if ($i === count($animalArray) - 1) {
+        $toOutput = $toOutput . '<p>' . $animalArray[$i] . ' ' . $secondNameArray[$arrayCount] . '</p>'; 
       }
       else {
-        echo $firstNameArray[$i] . $secondNameArray[$arrayCounter] . ', ';
+        $toOutput = $toOutput . '<p>' . $animalArray[$i] . ' ' . $secondNameArray[$arrayCount] . ',</p>';  
       }
-      $arrayCounter++; 
+      $arrayCount++;
     }
+    echo '<h2>' . $continentName . '</h2>'. $toOutput;
   }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -57,7 +61,22 @@
 <head>
   <meta charset="utf-8">
   <title>Fantastic Beast and where to find them</title>
+  <style>
+    body {
+      font-family: sans-serif;
+    }
+    h1, h2   {
+      text-transform: uppercase;
+      text-align: center;
+    }
+
+    p {
+      text-align: center; 
+    }
+  </style>
 </head>
 <body>
+  <h1>Fantastic Beast and where to find them</h1>
+  <?php fantasticBeast($continents) ?>
 </body>
 </html>
